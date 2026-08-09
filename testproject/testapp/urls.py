1d79c5e9dd38b18c44347630eb5e796f832abb44
katy24f234-sudo <katy24f234@gmail.com>
2026-08-09 18:51:56 +0330

    learned how to insret posts and read them and show them

--- testproject/testapp/urls.py
+++ testproject/testapp/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('all/',views.all_tabs),
+    path('all/',views.all_tabs,name='posts'),
     path('',views.home,name='home'),
     path('cart/',views.cart,name='cart'),
-    path('creatpost/',views.Post_create_form,name='creatpost')
+    path('creatpost/',views.Post_create_form,name='creatpost'),
+    path('post_details/<str:id>',views.post_details,name='post_details')
 ]
--- testproject/testapp/views.py
+++ testproject/testapp/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .forms import PostCreateForm
+from .models import Post
 
 def all_tabs(request):
-    return render(request,'testapp/all_tabs.html')
+    Posts =Post.objects.all()
+    data = {
+        'Posts':Posts
+    }
+    return render(request,'testapp/all_tabs.html',data)
 
 def home(request):
     return render(request,'testapp/home.html')
@@ -11,8 +16,20 @@ def cart(request):
     return render(request,'testapp/cart.html')
 
 def Post_create_form(request):
+    if(request.method == 'POST'):
+        form = PostCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts')
     form = PostCreateForm()
     data = {
         'form' : form
     }
     return render(request,'testapp/creatpost.html',data)
+
+def post_details(request,id):
+    post=Post.objects.get(pk=id)
+    data= {
+        'post':post
+    }
+    return render(request,'testapp/post_details.html',data)
